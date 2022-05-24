f86709cfe15e418c150e73a64a6848bf659d0c81
Vivek-jpg <57774129+theVivek-kumar@users.noreply.github.com>
2022-05-24 16:07:26 +0530

    fix

--- src/Components/Clock/Clock.jsx
+++ src/Components/Clock/Clock.jsx
@@ -1,8 +1,9 @@
 
  import { React, useState} from 'react'
  export default function Clock() {
-     const time = new Date().toLocaleTimeString();
-     const [currentTime, setCurrentTime] = useState(time)
+    const time = new Date().toLocaleTimeString();
+    const [currentTime, setCurrentTime] = useState(time)
+    console.log(currentTime)
 
      const updatedTime = () => {
          let time = new Date().toLocaleTimeString();
